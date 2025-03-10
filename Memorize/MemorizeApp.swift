8238aebd4fa17c3b330c637f7769b905a7a98aa5
JC Dela Cuesta <johndelacuesta@gmail.com>
2025-03-10 14:11:25 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by JC Dela Cuesta on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
